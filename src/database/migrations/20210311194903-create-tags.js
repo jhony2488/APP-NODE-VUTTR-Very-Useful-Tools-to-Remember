d3332d4f8306296a29e7960d3d3978a9b47c36a2
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-04-29 14:22:54 -0300

    migration tags created

---
+++ src/database/migrations/20210311194903-create-tags.js
@@ -0,0 +1,34 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('tags', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      slug: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('tags')
+  },
+}

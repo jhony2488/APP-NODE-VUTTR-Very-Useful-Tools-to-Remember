569841b0112be91f4f6692a5482a3490f5f02159
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-05-19 22:16:52 -0300

    reconfig swagger

--- swagger.ts
+++ swagger.ts
@@ -1,9 +1,22 @@
 const doc = require('./doc')
 const swaggerAutogen = require('swagger-autogen')()
 
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({
+    path: '.env.development',
+  })
+} else {
+  require('dotenv').config({
+    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+  })
+}
+
 const outputFile = 'swagger_output.json'
-const endpointsFiles = ['./src/routes.ts']
+const endpointsFiles = [
+  `./${process.env.ORIGIN_SEQUELIZE}/routes.${process.env.EXTENSION_FILES_ROUTER}`,
+]
 
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-  require('./src/server.ts')
+  require(`./${process.env.ORIGIN_SEQUELIZE}/server.${process.env.EXTENSION_FILES_ROUTER}`)
 })
+
